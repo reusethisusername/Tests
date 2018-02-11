35f8abb8e3cb3eb95f1b58c0120b8364078293a4
Marius D <marius.cristi@gmail.com>
2018-02-11 15:18:15 +0200

    Parse sample csv

--- Tests/MC_Sel.py
+++ Tests/MC_Sel.py
@@ -3,11 +3,28 @@ Created on Feb 6, 2018
 '''
 
 from selenium import webdriver
+import csv
+
+
+def parseFile(fname):
+    # assumption the csv file is in the same folder
+    with open(fname, 'r') as csv_file:
+        csv_reader = csv.DictReader(csv_file, delimiter=";")
+
+        for line in csv_reader:
+            print(line)
+
+
+def openSite(url):
+    '''open site here and do stuff'''
+    # url='https://primoco.me/app/booking'
+    # browser = webdriver.Chrome()
+    # browser.get(url)
 
 
 def main():
-    browser = webdriver.Chrome()
-    browser.get('https://primoco.me/app/booking')
+    openSite('http')
+    parseFile('sample.csv')
 
 
 if __name__ == '__main__':
